3020bb30d09777c1441b8f421ff2a2da74141035
tony55667788 <165141440+tony55667788@users.noreply.github.com>
2024-03-28 11:11:47 +0800

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Kasm',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://tony55667788.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/tony55667788/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
